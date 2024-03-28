576d81c979f8e2cc3d4693886dbfd2199190e601
scalar0114 <scalar0114@gmail.com>
2024-03-28 19:28:49 +0900

    add doc, exe files

--- ap2.c
+++ ap2.c
@@ -1,20 +1,29 @@
+// ap2.c와 마찬가지로 포인터 배열과 배열 포인터의 차이를 알려주는 프로그램
+// 정수형 배열과 정수형 포인터 배열을 선언, 값을 할당, 출력한다.
+// c언어에서 배열은 포인터로 사용할 수 있기 때문에 배열의 이름은 배열의 첫 번째 요소의 주소를 가리킨다.
+// 배열의 이름과 배열의 첫 번째 요소의 주소는 같다.
+// 배열의 이름에 정수를 더하면 배열의 첫 번째 요소의 주소에 정수를 더한 주소를 가리킨다.
+
 #include <stdio.h>
 #include <stdlib.h>
+
 void main()
 {
-    int list[5];
-    int *plist[5];
-    list[0] = 10;
-    list[1] = 11;
-    plist[0] = (int *)malloc(sizeof(int));
+    printf("[----- [hyunseung] [2023041001]] -----]\n");
+
+    int list[5]; // 정수형 배열 선언
+    int *plist[5]; // 정수형 포인터 배열 선언
+    list[0] = 10; // list 배열 첫 번째 요소에 10 할당
+    list[1] = 11; // list 배열 두 번째 요소에 11 할당
+    plist[0] = (int *)malloc(sizeof(int)); // plist 배열 첫 번째 요소에 동적 메모리 할당
     printf("list[0] \t= %d\n", list[0]);
     printf("list \t\t= %p\n", list);
     printf("&list[0] \t= %p\n", &list[0]);
-    printf("list + 0 \t= %p\n", list + 0);
+    printf("list + 0 \t= %p\n", list + 0);  
     printf("list + 1 \t= %p\n", list + 1);
     printf("list + 2 \t= %p\n", list + 2);
     printf("list + 3 \t= %p\n", list + 3);
     printf("list + 4 \t= %p\n", list + 4);
     printf("&list[4] \t= %p\n", &list[4]);
-    free(plist[0]);
+    free(plist[0]); // 동적메모리 해제
 }
--- p2-2.c
+++ p2-2.c
@@ -1,27 +1,39 @@
-#include <stdio.h>
-void print_one(int *ptr, int rows);
-int main()
-{
-    int one[] = {0, 1, 2, 3, 4};
-    printf("one = %p\n", one);
-    printf("&one = %p\n", &one);
-    printf("&one[0] = %p\n", &one[0]);
-    printf("\n");
-    printf("------------------------------------\n");
-    printf(" print_one(&one[0], 5) \n");
-    printf("------------------------------------\n");
-    print_one(&one[0], 5);
-    printf("------------------------------------\n");
-    printf(" print_one(one, 5) \n");
-    printf("------------------------------------\n");
-    print_one(one, 5);
-    return 0;
-}
-void print_one(int *ptr, int rows)
-{ /* print out a one-dimensional array using a pointer */
-    int i;
-    printf("Address \t Contents\n");
-    for (i = 0; i < rows; i++)
-        printf("%p \t %5d\n", ptr + i, *(ptr + i));
-    printf("\n");
-}
+// //p2-2.c는 배열의 이름과 배열의 첫 번째 요소의 주소를 출력하는 프로그램
+// //배열의 이름과 배열의 첫 번째 요소의 주소는 같다.
+// //배열의 이름에 정수를 더하면 배열의 첫 번째 요소의 주소에 정수를 더한 주소를 가리킨다.
+// //배열의 이름과 배열의 첫 번째 요소의 주소를 출력하는 함수를 작성한다.
+// //함수를 호출할 때 배열의 이름과 배열의 첫 번째 요소의 주소를 전달한다.
+// //함수에서는 배열의 이름과 배열의 첫 번째 요소의 주소를 출력한다.
+
+// #include <stdio.h>
+
+// void print_one(int *ptr, int rows);
+
+// int main()
+// {
+//     printf("[----- [hyunseung] [2023041001]] -----]\n");
+
+//     int one[] = {0, 1, 2, 3, 4};
+//     printf("one = %p\n", one);
+//     printf("&one = %p\n", &one);
+//     printf("&one[0] = %p\n", &one[0]);
+//     printf("\n");
+//     printf("------------------------------------\n");
+//     printf(" print_one(&one[0], 5) \n");
+//     printf("------------------------------------\n");
+//     print_one(&one[0], 5);
+//     printf("------------------------------------\n");
+//     printf(" print_one(one, 5) \n");
+//     printf("------------------------------------\n");
+//     print_one(one, 5);
+//     return 0;
+// }
+
+// void print_one(int *ptr, int rows)
+// { /* print out a one-dimensional array using a pointer */
+//     int i;
+//     printf("Address \t Contents\n");
+//     for (i = 0; i < rows; i++)
+//         printf("%p \t %5d\n", ptr + i, *(ptr + i));
+//     printf("\n");
+// }
